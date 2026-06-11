33e4b5afe6dd92ed813dfd1749a46ce87d8e4619
Julien Marx <julien.marx.2007@gmail.com>
2026-06-11 18:02:56 +0200

    fix: softmax broadcasting and implemented hardcoded version of crossentropy for MNIST

---
+++ src/femtotorch/loss.py
@@ -0,0 +1,17 @@
+import numpy as np
+from  femtotorch.tensor import Tensor
+
+ # loss functions using building block functions (essentially to check 
+
+
+def softmax(self):
+    shifted = (self - self.max(axis=-1, keepdims=True))
+    return shifted.exp() / (shifted.exp().sum(axis = -1, keepdims=True))
+
+#the tensor self is a batch of vectors of proba (batch_size * 10)
+#the tensor target is a batch of one-hot vectors (batch_size * 10) so it acts as a mask
+def crossEntropy_MNIST(self: Tensor, target: Tensor):
+    out = self * target # entry wise * so every element is 0 except the one hot 
+    out_scalar = out.max(axis = -1) # flatten every vector to a scalar which is the proba of the correct digit
+    return -(out_scalar.log())
+
