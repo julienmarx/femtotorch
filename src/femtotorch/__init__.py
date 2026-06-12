31c48959e8966f13c37f5caaf1893e2bacd190ea
Julien Marx <julien.marx.2007@gmail.com>
2026-06-13 00:54:24 +0200

    feat: made femtotorch a working package with imports in __init__.py

--- src/femtotorch/__init__.py
+++ src/femtotorch/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from femtotorch!"
+from femtotorch.tensor import Tensor
+from femtotorch.nn import MLP, Layer
+from femtotorch.optimizer import VanillaSGD
+from femtotorch.loss import crossEntropy_MNIST, softmax
+from femtotorch.datasets import load_mnist
+from femtotorch.dataloader import Dataloader
