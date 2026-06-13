019ad1423d785740a5ecdd66c8bff9dcbd05d6f9
Julien Marx <julien.marx.2007@gmail.com>
2026-06-13 12:27:26 +0200

    fix: crossentropy target type

--- src/femtotorch/loss.py
+++ src/femtotorch/loss.py
@@ -10,7 +10,7 @@ def softmax(self):
 
 #the tensor self is a batch of vectors of proba (batch_size * 10)
 #the tensor target is a batch of one-hot vectors (batch_size * 10) so it acts as a mask
-def crossEntropy_MNIST(self: Tensor, target: Tensor):
+def crossEntropy_MNIST(self: Tensor, target):
     out = self * target # entry wise * so every element is 0 except the one hot 
     out_scalar = out.max(axis = -1) # flatten every vector to a scalar which is the proba of the correct digit
     return -(out_scalar.log())
